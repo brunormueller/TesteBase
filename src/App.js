75356ed8839b4f987147ebba3bae637d63a7549a
VANDERSON MESKA <vanderson.meska@alunos.sc.senac.br>
2022-04-18 20:15:15 -0300

    commit dos material

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';		
+import '@fontsource/roboto/400.css';	
+import '@fontsource/roboto/500.css';	
+import '@fontsource/roboto/700.css';
 
 import CadastroUsusario from "./cadastro-usuario/CadastroUsuario"
 class App extends Component{
